0db10d84e71b7e86139f9b3248b38b393233793f
syeom04 <yeom.sy334@gmail.com>
2023-11-16 16:21:54 +0900

    update

--- Server/CV/yolov5/detect.py
+++ Server/CV/yolov5/detect.py
@@ -35,6 +35,25 @@ import platform
 import sys
 from pathlib import Path
 
+import numpy as np
+import cv2
+
+def get_limits(color):
+    
+    c = np.uint8([[color]])
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+    
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
+
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+
+
+
 import torch
 
 FILE = Path(__file__).resolve()
