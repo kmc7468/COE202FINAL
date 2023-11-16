9db13640f9ee7b2340bd56bce3da18a4e363b842
syeom04 <yeom.sy334@gmail.com>
2023-11-16 17:11:27 +0900

    1

--- Server/CV/yolov5/detect.py
+++ Server/CV/yolov5/detect.py
@@ -38,18 +38,18 @@ from pathlib import Path
 import numpy as np
 import cv2
 
-def get_limits(color):
+# def get_limits(color):
     
-    c = np.uint8([[color]])
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+#     c = np.uint8([[color]])
+#     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
     
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
+#     lowerLimit = hsvC[0][0][0] - 10, 100, 100
+#     upperLimit = hsvC[0][0][0] + 10, 255, 255
 
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
+#     lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+#     upperLimit = np.array(upperLimit, dtype=np.uint8)
 
-    return lowerLimit, upperLimit
+#     return lowerLimit, upperLimit
 
 
 
