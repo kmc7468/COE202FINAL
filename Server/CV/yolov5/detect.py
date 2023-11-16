57c5bab651548982bf67e4b8253380941d652e59
syeom04 <yeom.sy334@gmail.com>
2023-11-16 17:11:20 +0900

    1

--- Server/CV/yolov5/detect.py
+++ Server/CV/yolov5/detect.py
@@ -70,7 +70,6 @@ from utils.general import (LOGGER, Profile, check_file, check_img_size, check_im
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.torch_utils import select_device, smart_inference_mode
 
-
 @smart_inference_mode()
 def run(
         weights=ROOT / 'yolov5s.pt',  # model path or triton URL
