ea7a4f9b39a52f2782a58cbb489153bf5dfa1d24
static <kmc7468@naver.com>
2023-11-16 22:10:55 +0900

    AES256Cipher 최적화

--- Common/cipher.py
+++ Common/cipher.py
@@ -6,18 +6,18 @@ BLOCK_SIZE = 16
 
 class AES256Cipher:
 	def __init__(self, key: str | bytes, iv: bytes):
+		mykey = key
 		if type(key) is str:
-			self.__key = hashlib.sha256(key.encode("utf-8")).digest()
-		else:
-			self.__key = key
-		
-		self.__iv = iv
+			mykey = hashlib.sha256(key.encode("utf-8")).digest()
+
+		self.__cipher = AES.new(mykey, AES.MODE_CBC, iv)
+		self.__decipher = AES.new(mykey, AES.MODE_CBC, iv)
 
 	def encrypt(self, data: bytes) -> bytes:
-		return AES.new(self.__key, AES.MODE_CBC, self.__iv).encrypt(self.__pad(data))
+		return self.__cipher.encrypt(self.__pad(data))
 
 	def decrypt(self, edata: bytes) -> bytes:
-		return self.__unpad(AES.new(self.__key, AES.MODE_CBC, self.__iv).decrypt(edata))
+		return self.__unpad(self.__decipher.decrypt(edata))
 
 	def __pad(self, data: bytes) -> bytes:
 		padlen = BLOCK_SIZE - len(data) % BLOCK_SIZE
