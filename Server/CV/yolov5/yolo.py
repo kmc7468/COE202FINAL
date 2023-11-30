1012035392f2e61bcbf45a999bd8429ce68f8f0a
syeom04 <yeom.sy334@gmail.com>
2023-11-30 17:17:33 +0900

    yolo

--- Server/CV/yolov5/yolo.py
+++ Server/CV/yolov5/yolo.py
@@ -1,11 +1,23 @@
 import threading
 import detect
 
+class Yolo:
+    def __init__(self):
+        thread = threading.Thread(target=func, daemon=True)
+		thread.start()
+    
+    def start(self):
+        self.thread.start()
+    
+    def getObject(self):
+        return detect.objects[-1].objectName
+
+	def getCoordinates(self):
+    	return detect.objects[-1].coordinates
+
 def func():
 	detect.run(source=0)
-
 thread = threading.Thread(target=func, daemon=True)
-
 thread.start()
 
 def get():
