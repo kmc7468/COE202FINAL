6630af4df73942a2d8b834c232f1c86cb6ad63d6
syeom04 <yeom.sy334@gmail.com>
2023-12-01 16:34:12 +0900

    CV

--- Server/CV/yolov5/yolo.py
+++ Server/CV/yolov5/yolo.py
@@ -3,28 +3,30 @@ import detect
 
 class Yolo:
     def __init__(self):
-        thread = threading.Thread(target=func, daemon=True)
-		thread.start()
-    
+        self.thread = threading.Thread(target=self.func, daemon=True)
+        self.condition = threading.Condition()
+            
     def start(self):
         self.thread.start()
     
     def getObject(self):
         return detect.objects[-1].objectName
-
-	def getCoordinates(self):
-    	return detect.objects[-1].coordinates
-
-def func():
-	detect.run(source=0)
-thread = threading.Thread(target=func, daemon=True)
-thread.start()
+    
+    def getCoordinates(self):
+        return detect.objects[-1].coordinates
+    
+    def func(self):
+        detect.run(source = 0)
+ 
+yolo = Yolo()
+yolo.thread.start()
 
 def get():
 	return detect.objects[-1]
 
 input()
-print(get())
+# print(get())
+print(yolo.getObject(), yolo.getCoordinates())
 
 
     
